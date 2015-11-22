340c5718a44edde4aff32f7be44fe271df8bd978
Emmanuel Fuentes <eifuentes@Emmanuels-MacBook-Pro.local>
2015-11-22 12:30:31 -0800

    word2vec recipe feature matrix gen

--- feature_gen.py
+++ feature_gen.py
@@ -5,7 +5,7 @@ import multiprocessing
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
-
+from gensim.models import Word2Vec
 
 """ public variables """
 random_state = None  # random state, none -> random
@@ -34,10 +34,60 @@ def load_wc_data(path, encoder=None, shuffle=True):
     return df, encoder
 
 
-def main():
+""" build what's cooking gensim Word2Vec model """
+def build_wc_model(recipe_ingrdnts, size=100, n_jobs=1):
+    if verbose:
+        print 'training Word2Vec model on %s recipes using %s cores...' % (len(recipe_ingrdnts), n_jobs)
+    model = Word2Vec(recipe_ingrdnts, size=size, min_count=1, workers=n_jobs)
+    return model
+
+
+""" build what's cooking recipe vector representations aka feature matrix """
+def build_wc_vec(recipe_ingrdnts, model, size):
+    if verbose:
+        print 'building feature vectors...'
+    n_recipes = len(recipe_ingrdnts)
+    features_matrix = np.zeros((n_recipes, size), np.float32)
+    for idx_recipe, recipe in enumerate(recipe_ingrdnts):
+        n_ingrdnts = 0
+        for ingrdnt in recipe:
+            try:
+                features_matrix[idx_recipe, :] += model[ingrdnt]
+                n_ingrdnts += 1
+            except:
+                print 'error trying to transform %s' % ingrdnt
+                continue
+        features_matrix[idx_recipe, :] /= float(n_ingrdnts)
+    return features_matrix
+
+
+""" generate what's cooking feature matrix & components """
+def generate_wc_setup():
     n_cores = multiprocessing.cpu_count()
     print '\nloading training data...'
     train_df, cuisine_encoder = load_wc_data('data/train.json')
+    feature_vec_size = 100
+    wc_recipe_ingrdnts = train_df['ingredients']
+    print 'building size %s vectors...' % feature_vec_size
+    wc_features_model = build_wc_model(wc_recipe_ingrdnts,
+        size=feature_vec_size, n_jobs=n_cores)
+    wc_features = build_wc_vec(wc_recipe_ingrdnts,
+        wc_features_model, feature_vec_size)
+    return {
+        'train': {
+            'df': train_df,
+            'features_matrix': wc_features,
+        },
+        'label_encoder': cuisine_encoder,
+        'model': wc_features_model
+    }
+
+
+""" main entry method """
+def main():
+    wc_components = generate_wc_setup()
+    print 'what\'s cooking features matrix of dim %s by %s' % wc_components['train']['features_matrix'].shape
+
 
 if __name__ == '__main__':
     main()
